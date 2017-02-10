879aaf24b0eb8ff0eb79adb245ca762283e00123
Neil Satterfield <neilsatt@gmail.com>
2017-02-09 19:49:20 -0700

    objects

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-// Object Literal
+// Object Literal - with key/value pairs
 var person = {
     name: 'Ted',
     state: 'Idaho',
@@ -6,3 +6,28 @@ var person = {
     collegeStudent: true,
     courses : ['math', 'science', 'music']
 };
+
+
+// Accessing the value
+alert(person['name']);
+alert(person.name); // more common
+
+// change a property
+person.name = "Bill";
+
+// Looping through key values in an object
+// You can use any variable in places of 'key'
+for( var key in person) {
+    alert(key); // alerts property names
+}
+
+// Looping through the property values 
+for( var key in person) {
+    alert(person[key]); // alerts out property value, you can't use dot notation here
+}
+
+
+// Alternate way 
+for( var key in person) {
+    console.log(key, ': ', person[key]); // logs out keys and values, again you can't use dot notation
+}
